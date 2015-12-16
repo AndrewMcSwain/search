d0d266238367c48b347bd76aa2f87eb467a7699d
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2015-12-16 01:18:24 -0200

    stringIndex functionality
    what does it do? is it a string or a number? why wont this pos code run??????

--- code.js
+++ code.js
@@ -1,58 +1,9 @@
-function findLetter(letter){
-  
-  //get the letter
-  letter.toString();
-  var text = prompt("enter your text","");
-  
-  // loop length needs to be as long as text length
-  var textLength = text.length; 
-  
-  //index position
-  var dickSize = 0;
-  var dick = 0;
-  
-  //array to store index values of where letter was found
-  var inpussy = [];
- 
-  // for fun
-  function grow(){dickSize++;}
-  
-  //the mechanism to search for the letter and store the index
- function getSome(){
-   while (dickSize < textLength){
-      var dick = text.indexOf(letter, dickSize);
-      if (dickSize == dick){
-  	  	inpussy.push(dick);}
-      grow();  
- 	}
-   return inpussy;
- };
-return getSome();
 
-var matchWords = [];  
- 
 function getArray(){
-	var stringArray = text.split(" ");
+var text = "You can use this page to download source code and solutions to exercises for the book Eloquent JavaScript, and to directly run code in the context of chapters from that book, either to solve exercises to simply play around.";
+var stringArray = text.split(" ");
   	for (i = 0; i < stringArray.length; i++){
-      	var stringIndex = stringArray[i];c
-	for (b = 0; b < stringIndex.length; b++ )
-
-	
-	 {
-		  var itemSearch = stringIndex.indexOf(letter, b);
-		  if (stringIndex >=0 ){
-        		matchWords.push([stringIndex);
-	 		}	
-      }
-      return  
-     }
-};
-                         
-                         
-console.log(getSome());
-console.log(getArray());  
-
-  
-  
-};
-
+      	var stringIndex = stringArray[i];
+      console.log(stringIndex);
+      }}
+                  
